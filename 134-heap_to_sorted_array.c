e4782ba6bcd6836b86eded9741e1103b8156d8fc
Javanseal <agomjavan@gmail.com>
2024-06-06 14:37:28 -0700

    Task 40

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * heap_to_sorted_array - to convert a Binary Max Heap to a sorted array of int
+ * @heap: the root of the tree
+ * @size: the size from array
+ * Return: pointer
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (NULL);
+}
